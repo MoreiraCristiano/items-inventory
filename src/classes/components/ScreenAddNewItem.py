a330a835e03713ad4b3d52f4d2a7e5e9732faf25
cristiano m <moreiracristiano702@gmail.com>
2024-02-12 21:05:53 -0300

    add dropdown menu to select between categories

--- src/classes/components/ScreenAddNewItem.py
+++ src/classes/components/ScreenAddNewItem.py
@@ -1,6 +1,7 @@
 from flet import (
     Column,
-    MainAxisAlignment,
+    Dropdown,
+    dropdown,
     UserControl,
     TextField,
     DatePicker,
@@ -16,34 +17,41 @@ from flet import (
 from sqlalchemy.orm import Session
 from datetime import datetime
 from model.InventoryItem import InventoryItem
+from .ScreenCategories import ScreenCategories
 
 
 class ScreenAddNewItem(UserControl):
     def __init__(self, page, database_engine):
         super().__init__()
         self.page = page
+        self.engine = database_engine
+        self.categories_instance = ScreenCategories(page, database_engine)
         self.item = TextField(label='Item', icon=icons.CREATE_SHARP)
-        self.category = TextField(label='Category', icon=icons.LABEL)
+        self.category = Dropdown(
+            icon=icons.LABEL,
+            hint_text='Category',
+            options=[
+                dropdown.Option(category)
+                for category in self.categories_instance.get_distinct_categories()
+            ],
+        )
         self.extra_info = TextField(label='Extra information', icon=icons.INFO)
-        self.btn_home = ElevatedButton(
-            'Go Home', on_click=lambda _: self.page.go('/'), height=45
-        )
-        self.btn_save = ElevatedButton(
-            'Save', height=45, on_click=lambda e: self.save_item(e)
-        )
         self.date_picker = DatePicker(
             on_change=self.change_date,
             first_date=datetime(2023, 10, 1),
             last_date=datetime(2024, 10, 1),
         )
 
+        self.btn_save = ElevatedButton(
+            'Save', height=45, on_click=lambda e: self.save_item(e)
+        )
         self.date_button = ElevatedButton(
             'Expiration date',
             icon=icons.CALENDAR_MONTH,
             on_click=lambda _: self.date_picker.pick_date(),
             height=50,
         )
-        self.engine = database_engine
+
         page.overlay.append(self.date_picker)
 
     def set_controls_disable(self, switch):
@@ -53,7 +61,6 @@ class ScreenAddNewItem(UserControl):
         Return: Null
         '''
         self.btn_save.disabled = switch
-        # self.btn_home.disabled = switch
         self.item.disabled = switch
         self.category.disabled = switch
         self.extra_info.disabled = switch
